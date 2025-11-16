f975ca66c2d1272db5f47ae1a58353139368544d
Richard Reyes <richard080405@gmail.com>
2025-11-16 12:00:19 -0500

    Añadido empleado por comisión y validaciones.

---
+++ ValidationException.java
@@ -0,0 +1,7 @@
+package com.udc.act3.sistemanomina;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
