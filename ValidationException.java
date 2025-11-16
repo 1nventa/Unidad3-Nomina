051989d9f242431bda947e537dfd859c2a53b6f2
Richard Reyes <richard080405@gmail.com>
2025-11-16 12:07:02 -0500

    Delete ValidationException.java

--- ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.udc.act3.sistemanomina;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
